d7fcdd4879ad5066d7584f11823204d970ef6878
Giovani Cassiano Nogueira <giovani.cassiano@outlook.com>
2022-05-27 00:27:26 -0300

    chore: tests of wppMeta

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,3 @@
 import { WppMeta } from './core/providers/wppmeta/wppmeta'
 
-const token =
-  'EAAKk8rcIctoBAH0wtnjW3kDQZCGg7VsguK5yoj2qOZCZCD3jaZCli6taY6ObMdA3DgvezfyYFYPcsPEhpiMbVcwCOKuIH668W5AhCclyf3WXJ5V1mjsrXzTjRnD5TsPZC3v1lPrCDeSK6j5ii8ZAeF3v0sTUOe6nXtVDfruXPYnfwr1aniO1XLV3vyZAdBUKwdkVwsQ3fB5AQZDZD'
-const whatsApp = WppMeta.config({
-  token,
-  accountId: '108299961890984',
-  phoneId: '103858795675152',
-})
-
-whatsApp
-  .sendLocation({
-    to: '5517992230802',
-    location: {
-      longitude: '-20.7751211',
-      latitude: '-49.4194561',
-      name: 'Giovani',
-      address: 'Rua',
-    },
-  })
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error))
+export { WppMeta }
